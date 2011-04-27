0ebd07de937f6c067d7147ec897b6262fe14d23c
amaurremi <amaurremi@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-04-27 19:39:44 +0000

    Warning flag

--- haskell/err_test.hs
+++ haskell/err_test.hs
@@ -3,28 +3,34 @@ import HscTypes
 import ErrUtils
 import Bag
 import Outputable
-import SrcLoc
 import MonadUtils
 import System.Environment
+import SrcLoc
+import Data.Maybe
+import FastString
 
 locStr :: SrcLoc -> String
 locStr loc = if isGoodSrcLoc loc then show (srcLocLine loc) ++ ":" ++ show (srcLocCol loc) else "?"
 
 spanStr :: SrcSpan -> String
-spanStr span = locStr (srcSpanStart span) ++ "-" ++ locStr (srcSpanEnd span) ++ " " ++ show (srcSpanFileName_maybe span)
+spanStr span = locStr (srcSpanStart span) ++ "-" ++ locStr (srcSpanEnd span)
 
 msgStr :: Message -> PrintUnqualified -> String
 msgStr msg unqual = show $ msg (mkErrStyle unqual)
 
 newMsgIndicator = "\f"
+badSrcSpan       = "\b"
 
 output1 :: (MonadIO m) => ErrMsg -> m ()
 output1 msg = do
-    liftIO $ putStrLn newMsgIndicator
-    liftIO $ putStrLn $ spanStr $ head $ errMsgSpans msg
-    let unqual = errMsgContext msg
-    liftIO $ putStrLn $ msgStr (errMsgShortDoc msg) unqual
-    liftIO $ putStrLn $ msgStr (errMsgExtraInfo msg) unqual
+    let span   = head $ errMsgSpans msg
+        unqual = errMsgContext msg
+        lifty  = liftIO . putStrLn
+    lifty newMsgIndicator
+    lifty $ show $ fromMaybe (mkFastString badSrcSpan) $ srcSpanFileName_maybe span
+    lifty $ spanStr span
+    lifty $ msgStr (errMsgShortDoc msg) unqual
+    lifty $ msgStr (errMsgExtraInfo msg) unqual
 
 outputBag :: (MonadIO m) => Bag ErrMsg -> m ()
 outputBag msgs = do
@@ -47,8 +53,7 @@ doWalk :: [String] -> [String] -> Ghc ()
 doWalk cmdFlags files = do
     flg <- getSessionDynFlags
     (flg, _, _) <- parseDynamicFlags flg (map noLoc cmdFlags)
-    --setSessionDynFlags (flg { hscTarget = HscNothing, ghcLink = NoLink })
-    setSessionDynFlags flg
+    setSessionDynFlags (flg { hscTarget = HscNothing, ghcLink = NoLink })
     mapM_ (\file -> addTarget Target { targetId = TargetFile file Nothing, targetAllowObjCode = False, targetContents = Nothing }) files
     loadWithLogger logger LoadAllTargets `gcatch` catcher
     warns <- getWarnings
@@ -57,5 +62,5 @@ doWalk cmdFlags files = do
 
 main = do
     args     <- getArgs
-    runGhc (Just "C:/Haskell/lib") (doWalk ["-W"] args)
+    runGhc (Just "C:/Program Files (x86)/Haskell Platform/2010.2.0.0/lib/") (doWalk ["-W"] args)
     return ()
