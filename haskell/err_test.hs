42e1bb934feeb2c75c171ff0b9181d0528b1553a
amaurremi <amaurremi@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-05-02 21:03:24 +0000

    Command line options

--- haskell/err_test.hs
+++ haskell/err_test.hs
@@ -8,12 +8,17 @@ import System.Environment
 import SrcLoc
 import Data.Maybe
 import FastString
+import Control.Monad (when)
+import Data.Char (isUpper)
+import System.Console.GetOpt
 
 locStr :: SrcLoc -> String
-locStr loc = if isGoodSrcLoc loc then show (srcLocLine loc) ++ ":" ++ show (srcLocCol loc) else "?"
+locStr loc = if isGoodSrcLoc loc then show (srcLocLine loc) ++ ":"
+  ++ show (srcLocCol loc) else "?"
 
 spanStr :: SrcSpan -> String
-spanStr span = locStr (srcSpanStart span) ++ "-" ++ locStr (srcSpanEnd span)
+spanStr span = locStr (srcSpanStart span) ++ "-"
+  ++ locStr (srcSpanEnd span)
 
 msgStr :: Message -> PrintUnqualified -> String
 msgStr msg unqual = show $ msg (mkErrStyle unqual)
@@ -27,7 +32,8 @@ output1 msg = do
         unqual = errMsgContext msg
         lifty  = liftIO . putStrLn
     lifty newMsgIndicator
-    lifty $ show $ fromMaybe (mkFastString badSrcSpan) $ srcSpanFileName_maybe span
+    lifty $ show $ fromMaybe (mkFastString badSrcSpan)
+          $ srcSpanFileName_maybe span
     lifty $ spanStr span
     lifty $ msgStr (errMsgShortDoc msg) unqual
     lifty $ msgStr (errMsgExtraInfo msg) unqual
@@ -53,14 +59,79 @@ doWalk :: [String] -> [String] -> Ghc ()
 doWalk cmdFlags files = do
     flg <- getSessionDynFlags
     (flg, _, _) <- parseDynamicFlags flg (map noLoc cmdFlags)
-    setSessionDynFlags (flg { hscTarget = HscNothing, ghcLink = NoLink })
-    mapM_ (\file -> addTarget Target { targetId = TargetFile file Nothing, targetAllowObjCode = False, targetContents = Nothing }) files
+    setSessionDynFlags
+      (flg { hscTarget = HscNothing, ghcLink = NoLink })
+    mapM_ (\file -> addTarget Target {
+        targetId = TargetFile file Nothing
+      , targetAllowObjCode = False
+      , targetContents = Nothing })
+      files
     loadWithLogger logger LoadAllTargets `gcatch` catcher
     warns <- getWarnings
     outputBag warns
     return ()
 
+-- ./err_test
+--    -g <path>           # ghc path
+--    -o <path>           # output path
+--    -c "<options>"      # compiler options
+--    -e <file>           # source file to be made executable
+--    <files>             # files to be compiled
+
+data Options = Options
+  { ghcPath         :: String
+  , outputPath      :: String
+  , compilerOptions :: [String]
+  , exeFile         :: Maybe String
+  } deriving Show
+
+options :: [OptDescr (Options -> Options)]
+options =
+  [ Option ['g'] ["ghcpath"]    (ReqArg (\path opt -> opt {ghcPath = path}) "DIR")
+    "GHC path"
+  , Option ['o'] ["outpath"]    (ReqArg (\path opt ->
+    opt {outputPath = path}) "DIR") "output path"
+  , Option ['c'] ["ghcoptions"] (ReqArg (\opts opt ->
+    opt {compilerOptions = words opts}) "STRING") "GHC options"
+      -- need to delete quotes?
+      -- STRING possible here?
+  , Option ['e'] ["toexe"]      (ReqArg (\file opt -> opt {exeFile = Just file}) "FILE")
+    "file to be make executable"
+  ]
+
+defaultOpts :: Options
+defaultOpts = Options
+  { ghcPath         = "./"
+  , outputPath      = "./"
+  , compilerOptions = []
+  , exeFile         = Nothing
+  }
+
 main = do
-    args     <- getArgs
-    runGhc (Just "C:/Program Files (x86)/Haskell Platform/2010.2.0.0/lib/") (doWalk ["-W"] args)
+    args <- getArgs
+    let (opts', files, errors)     = getOpt Permute options args
+    when (not (null errors)) $ ioError $ userError $ concat errors
+    let opts                      = foldl (\opt f -> f opt) defaultOpts opts'
+        outPath                   = outputPath opts
+        makeSrcs                  = map (++ ".hs")
+        makeObjects               = map (++ ".o")
+        (modules, nonModules)        = foldl (\(ups, lows)
+          file@(f:_) -> if isUpper f then (file:ups, lows) else (ups, file:lows))
+          ([], []) files
+        runGhcPath additionalOpts = runGhc (Just (ghcPath opts))
+          . doWalk (compilerOptions opts ++ additionalOpts)
+    runGhcPath ["--make", "-c", "-outputdir " ++ outPath] $ makeSrcs modules
+    mapM (\file -> runGhcPath ["-c", "-i" ++ outPath
+      , "-o " ++ outPath ++ "/" ++ file ++ ".o"
+      , "-ohi " ++ outPath ++ "/" ++ file ++ ".hi"]
+      [file])
+      $ makeObjects nonModules
+    case exeFile opts of
+      Just toExe -> runGhcPath
+        ["-o " ++ outPath ++ "/" ++ toExe ++ ".exe", toExe ++ ".o"]
+        [toExe]
+      _          -> return ()
     return ()
+
+-- runGhc (Just "C:/Program Files (x86)/Haskell Platform/2010.2.0.0/lib/")
+-- (doWalk ["-W"] args)
