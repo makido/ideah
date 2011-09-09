577ca06ec95592128603dd2df4dd1075b25de424
amaurremi <amaurremi@9f1b87a4-9290-56fd-1e0b-2b6658918edd>
2011-09-09 15:03:40 +0000

    Module import fixed

--- haskell/CheckMain.hs
+++ haskell/CheckMain.hs
@@ -4,20 +4,29 @@ import GHC
 import Outputable
 import MonadUtils
 import DynFlags
+import System.FilePath (equalFilePath)
+import System.Directory (canonicalizePath)
+import Control.Monad (filterM)
 
 checkMain srcpath file = do
-  flags <- getSessionDynFlags
+  flags          <- getSessionDynFlags
   (flags', _, _) <- parseDynamicFlags flags (map noLoc ["-i" ++ srcpath])
-  setSessionDynFlags (flags' { hscTarget    = HscNothing
-                             , ghcLink      = NoLink})
+  setSessionDynFlags (flags'
+    { hscTarget          = HscNothing
+    , ghcLink            = NoLink})
   addTarget Target
-    { targetId = TargetFile file Nothing
+    { targetId           = TargetFile file Nothing
     , targetAllowObjCode = False
-    , targetContents = Nothing }
-  (summary : _) <- depanal [] False
-  parsedMod     <- parseModule summary
-  let decls     = hsmodDecls $ unLoc $ parsedSource parsedMod
-  let hasMain = any isMain $ map unLoc decls
+    , targetContents     = Nothing }
+  summaries <- depanal [] False
+  summy <- filterM (\sum -> do
+      absoluteSummary <- liftIO $ canonicalizePath $ ms_hspp_file sum
+      absoluteFile    <- liftIO $ canonicalizePath file
+      return $ equalFilePath absoluteFile absoluteSummary)
+    summaries
+  parsedMod      <- parseModule $ head summy
+  let decls      = hsmodDecls $ unLoc $ parsedSource parsedMod
+  let hasMain    = any isMain $ map unLoc decls
   liftIO $ putStrLn $ if hasMain then "t" else "f"
 
 isMain (ValD (FunBind funid _ _ _ _ _)) = showSDoc (ppr $ unLoc funid) == "main"
